0c4af098cc4861a1a332d4feed25ae06ea45b4ba
Stuart McFarlane <stuart@mcfarlane.nl>
2024-11-06 10:40:30 +0100

    Course.List done

--- src/Course/List.hs
+++ src/Course/List.hs
@@ -76,8 +76,8 @@ headOr ::
   a
   -> List a
   -> a
-headOr =
-  error "todo: Course.List#headOr"
+headOr a Nil = a
+headOr _ (head :. _) = head
 
 -- | The product of the elements of a list.
 --
@@ -92,8 +92,7 @@ headOr =
 product ::
   List Int
   -> Int
-product =
-  error "todo: Course.List#product"
+product = foldRight (*) 1
 
 -- | Sum the elements of the list.
 --
@@ -107,8 +106,7 @@ product =
 sum ::
   List Int
   -> Int
-sum =
-  error "todo: Course.List#sum"
+sum = foldRight (+) 0
 
 -- | Return the length of the list.
 --
@@ -119,8 +117,7 @@ sum =
 length ::
   List a
   -> Int
-length =
-  error "todo: Course.List#length"
+length = foldRight (\_ len -> len + 1) 0
 
 -- | Map the given function on each element of the list.
 --
@@ -134,8 +131,8 @@ map ::
   (a -> b)
   -> List a
   -> List b
-map =
-  error "todo: Course.List#map"
+map _ Nil = Nil
+map fn (head :. tail) = (fn head) :. map fn tail
 
 -- | Return elements satisfying the given predicate.
 --
@@ -151,8 +148,8 @@ filter ::
   (a -> Bool)
   -> List a
   -> List a
-filter =
-  error "todo: Course.List#filter"
+filter _ Nil = Nil
+filter pred (head :. tail) = if (pred(head)) then (head :. filter pred tail) else (filter pred tail)
 
 -- | Append two lists to a new list.
 --
@@ -170,8 +167,9 @@ filter =
   List a
   -> List a
   -> List a
-(++) =
-  error "todo: Course.List#(++)"
+(++) as Nil = as
+(++) Nil bs = bs
+(++) as bs = foldRight (\a acc -> (a :. acc)) bs as
 
 infixr 5 ++
 
@@ -188,8 +186,7 @@ infixr 5 ++
 flatten ::
   List (List a)
   -> List a
-flatten =
-  error "todo: Course.List#flatten"
+flatten = foldRight (\la acc -> la ++ acc) Nil
 
 -- | Map a function then flatten to a list.
 --
@@ -205,8 +202,7 @@ flatMap ::
   (a -> List b)
   -> List a
   -> List b
-flatMap =
-  error "todo: Course.List#flatMap"
+flatMap f as = flatten (map f as)
 
 -- | Flatten a list of lists to a list (again).
 -- HOWEVER, this time use the /flatMap/ function that you just wrote.
@@ -215,8 +211,7 @@ flatMap =
 flattenAgain ::
   List (List a)
   -> List a
-flattenAgain =
-  error "todo: Course.List#flattenAgain"
+flattenAgain as = flatMap (\aa -> aa) as
 
 -- | Convert a list of optional values to an optional list of values.
 --
@@ -240,8 +235,9 @@ flattenAgain =
 seqOptional ::
   List (Optional a)
   -> Optional (List a)
-seqOptional =
-  error "todo: Course.List#seqOptional"
+seqOptional Nil                 = Full Nil
+seqOptional (Empty :. _)        = Empty
+seqOptional (Full item :. tail) = mapOptional (item :.) (seqOptional tail)
 
 -- | Find the first element in the list matching the predicate.
 --
@@ -263,8 +259,8 @@ find ::
   (a -> Bool)
   -> List a
   -> Optional a
-find =
-  error "todo: Course.List#find"
+find _ Nil = Empty
+find pred (head :. tail) = if (pred(head)) then (Full head) else find pred tail
 
 -- | Determine if the length of the given list is greater than 4.
 --
@@ -282,8 +278,8 @@ find =
 lengthGT4 ::
   List a
   -> Bool
-lengthGT4 =
-  error "todo: Course.List#lengthGT4"
+lengthGT4 (_ :. _ :. _ :. _ :. _ :. _) = True
+lengthGT4 _ = False
 
 -- | Reverse a list.
 --
@@ -299,8 +295,7 @@ lengthGT4 =
 reverse ::
   List a
   -> List a
-reverse =
-  error "todo: Course.List#reverse"
+reverse = foldLeft (\acc a -> (a :. acc)) Nil
 
 -- | Produce an infinite `List` that seeds with the given value at its head,
 -- then runs the given function for subsequent elements
@@ -314,8 +309,9 @@ produce ::
   (a -> a)
   -> a
   -> List a
-produce f x =
-  error "todo: Course.List#produce"
+produce f x = 
+  let prod a = a :. prod (f a)
+  in prod x
 
 -- | Do anything other than reverse a list.
 -- Is it even possible?
@@ -329,8 +325,9 @@ produce f x =
 notReverse ::
   List a
   -> List a
-notReverse =
-  error "todo: Is it even possible?"
+notReverse as = as
+
+-- Not sure what they are looking for here. It trivially seems possible as shown above.
 
 ---- End of list exercises
 
